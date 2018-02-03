e878706b8f42c9c180567b9ea7d840aefffdd5df
nicholasadamou <nicholasadamouemail@gmail.com>
2018-02-03 12:31:19 -0500

    updated website to new re-design

--- src/js/app.js
+++ src/js/app.js
@@ -35,6 +35,7 @@ function toggleNav() {
   var $hamburger = $(".hamburger");
   var $link = $(".nav_link");
   var $logo = $(".logo");
+  var $body = $("body");
   
   $hamburger.bind('click', function() {
     $hamburger.toggleClass("is-active");
@@ -52,6 +53,12 @@ function toggleNav() {
   $logo.bind("click", function() {
     $hamburger.removeClass("is-active");
     $('body').removeClass('nav-open');
+    this.blur();
+  });
+
+  $body.bind("click", function() {
+    $body.removeClass("nav-open");
+    $hamburger.removeClass("is-active");
     this.blur();
   });
 }
