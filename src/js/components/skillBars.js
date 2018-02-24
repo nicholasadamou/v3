ec05cda66c0a75d2a08bfe1597a04074ea586414
nicholasadamou <nicholasadamouemail@gmail.com>
2018-02-24 18:18:53 -0500

    revamped various sections

--- src/js/components/skillBars.js
+++
@@ -1,6 +0,0 @@
-function animate_skill_bars() {
-	$('.bar').each(function() {
-		var max = $(this).attr('width');
-		$(this).animate({ width: max }, { duration: 2000 });
-	});
-}
--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,9 @@
 //=include components/smoothScroll.js
 //=include components/toggleNav.js
 //=include components/hero.js
-//=include components/skillBars.js
 
 $(function() {
 	hero();
-	animate_skill_bars();
 
 	toggleNav();
 
