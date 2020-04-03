a3d74ed72ff71200c177166cfb3ef4d996da875e
Nicholas Adamou <nicholasadamouemail@gmail.com>
2020-04-02 21:19:37 -0400

    added surge git pre-push hook

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.35f3a112eb9258f975e046a9f9500ae7.js"
+  "/precache-manifest.94e411c81664909a3756037e63bf9297.js"
 );
 
 self.addEventListener('message', (event) => {
