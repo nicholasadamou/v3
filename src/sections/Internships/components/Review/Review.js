f1d5856c1b36db6b024fd0e900dcce7fa9492ebc
Nicholas Adamou <nicholasadamouemail@gmail.com>
2020-04-10 21:54:43 -0400

    added review sub-section

---
+++ src/sections/Internships/components/Review/Review.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import './index.scss'
+
+const Review = (author, source, quote, image) => (
+	<div className="review">
+		<blockquote className="review-quote">
+			<q>
+				"{quote}"
+			</q>
+			<div className="review-footer">
+				<img
+					src={image}
+					alt="avatar"
+					className="review-image"
+				/>
+				<div className="review-cite">
+					<span className="review-author">{author}</span>
+					<span className="review-source">{source}</span>
+				</div>
+			</div>
+		</blockquote>
+	</div>
+)
+
+export default Review
