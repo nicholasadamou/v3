4e83845350fcc617f2cfeddae11a5518fff0411b
Nicholas Adamou <nicholasadamouemail@gmail.com>
2020-05-23 07:28:20 -0400

    fixed issue with website.js

--- functions/website/website.js
+++ functions/website/website.js
@@ -1,27 +1,26 @@
 const chromium = require("chrome-aws-lambda");
 const devices = require('puppeteer/DeviceDescriptors');
-const {URL} = require('url');
+const validator = require('validator');
 
 const cache = new Map();
 
-function isValidURL(str) {
-	try {
-		return new URL(str).hostname.includes('.');
-	} catch (e) {
-		console.error(e.message);
-		return false;
-	}
+function isOneOfMySites(url) {
+	return [
+		'https://cutpastecopy.github.io',
+		'https://nicholasadamou.com',
+		'https://advanced-electrical-services.netlify.app'
+	].includes(url);
 }
 
-async function getOptions(isDev) {
-	return isDev 
-		? 
+async function getOptions() {
+	return process.env.URL.includes("http://localhost")
+		?
 			{
 				product: "chrome",
 				args: [],
 				executablePath: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
 				headless: true,
-			};
+			}
 		:
 			{
 				product: "chrome",
@@ -31,13 +30,13 @@ async function getOptions(isDev) {
 			};
 }
 
-async function getScreenshot(url, device = "desktop", type = 'png', isDev) {
+async function getScreenshot(url, device = "desktop", type = 'png') {
 	console.log({url, device, type})
 
-	const options = await getOptions(isDev);
+	const options = await getOptions();
 	const browser = await chromium.puppeteer.launch(options);
 	const page = await browser.newPage();
-	
+
 	await chromium.font('https://raw.githack.com/googlei18n/noto-emoji/master/fonts/NotoColorEmoji.ttf');
 
 	if (device === "mobile") {
@@ -67,49 +66,59 @@ exports.handler = async (event, context) => {
 	const {url, device = 'desktop', type = 'png'} = event.queryStringParameters;
 
 	try {
-		if (isValidURL(`https://${url}/`)) {
-			let screenshot = '';
-			const key = `https://${url}/:${device}`;
+		if (validator.isURL(`https://${url}/`, {
+			protocols: ['http', 'https']
+		})) {
+			if (isOneOfMySites(`https://${url}/`)) {
+				let screenshot = '';
+				const key = `https://${url}/:${device}`;
 
-			if (cache.has(key)) {
-				screenshot = await cache.get(key);
-			} else {
-				let prefix = `${process.env.URL}/.netlify/functions/`
+				if (cache.has(key)) {
+					screenshot = await cache.get(key);
+				} else {
+					let prefix = `${process.env.URL}/.netlify/functions/`
 
-				if (process.env.URL.includes("http://localhost")) {
-					prefix = `${process.env.URL}`
+					if (process.env.URL.includes("http://localhost")) {
+						prefix = `${process.env.URL}`
+					}
+
+					console.log(
+						`${prefix}/website?${new URLSearchParams(event.queryStringParameters).toString()}`
+					);
+
+					screenshot = await getScreenshot(
+						`https://${url}/`,
+						`${device}`,
+						`${type}`
+					);
+
+					cache.set(key, screenshot);
 				}
 
-				console.log(
-					`${prefix}/website?${new URLSearchParams(event.queryStringParameters).toString()}`
-				);
+				console.log({
+					statusCode: 200,
+					'Content-Type': `image/${type}`,
+					'Content-Length': screenshot.length,
+				});
 
-				screenshot = await getScreenshot(
-					`https://${url}/`,
-					`${device}`,
-					`${type}`,
-					process.env.URL.includes("http://localhost")
-				);
-
-				cache.set(key, screenshot);
+				return {
+					statusCode: 200,
+					body: screenshot,
+					isBase64Encoded: true
+				};
+			} else {
+				return {
+					statusCode: 400,
+					body: JSON.stringify({
+						message: `Bad Request: The provided URL ${url} is not one of Nicholas Adamou's websites.`
+					})
+				}
 			}
-
-			console.log({
-				statusCode: 200,
-				'Content-Type': `image/${type}`,
-				'Content-Length': screenshot.length,
-			});
-
-			return {
-				statusCode: 200,
-				body: screenshot,
-				isBase64Encoded: true
-			};
 		} else {
 			return {
 				statusCode: 400,
 				body: JSON.stringify({
-					message: `Bad Request: The url ${url} is not valid.`
+					message: `Bad Request: The provided URL ${url} is not valid.`
 				})
 			}
 		}
@@ -117,7 +126,7 @@ exports.handler = async (event, context) => {
 		return {
 			statusCode: 500,
 			body: JSON.stringify({
-				message: `${e.message}`
+				message: `Server Error: ${e.message}`
 			})
 		}
 	}
