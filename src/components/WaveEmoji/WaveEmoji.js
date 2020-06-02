8d12e775b81380d5a2ca34da402f63490f7a20b6
Nicholas Adamou <nicholasadamouemail@gmail.com>
2020-06-02 17:11:33 -0400

    updated Logo.js and Footer.js

--- src/components/WaveEmoji/WaveEmoji.js
+++ src/components/WaveEmoji/WaveEmoji.js
@@ -1,30 +1,9 @@
 import React from "react";
 
-import "./index.scss";
-
-import styled from "styled-components";
-
-const Emoji = styled.span`
-  display: inline-block;
-
-  -webkit-animation-duration: 1.8s;
-
-  animation-duration: 1.8s;
-  -webkit-animation-iteration-count: infinite;
-  animation-iteration-count: infinite;
-  -webkit-animation-name: wave;
-  animation-name: wave;
-
-  -webkit-transform-origin: 70% 70%;
-
-  transform-origin: 70% 70%;
-`;
-
 const WaveEmoji = () => (
-  // eslint-disable-next-line jsx-a11y/accessible-emoji
-  <Emoji className="wave-emoji" role="img" aria-label="wave">
+  <span role="img" aria-label="wave">
     👋🏼
-  </Emoji>
+  </span>
 );
 
 export default WaveEmoji;
