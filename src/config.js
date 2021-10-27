270158e24a4571641393f3e0b917544113c458a4
Nicholas Adamou <nicholasadamouemail@gmail.com>
2021-10-26 21:52:19 -0400

    updated job descriptions

--- src/config.js
+++ src/config.js
@@ -36,7 +36,7 @@ const config = {
 			duration: 'May 2021 - Present',
 			location: 'Southbury, CT',
 			description:
-				"Leads the software development team for IBM's cloud-native ledger software, GLUI-NG. Works with developers, UI/UX designers, PO's, IM's, testers, and users across cross-functional teams to enhance the journaling software. Leverages modern technologies such as PostgreSQL, GraphQL, Apollo, MQ, Apache POI, Go, and IBM Hybrid Cloud to bring a new modern face to IBM's ledger application. Acts as the lead librarian for GUDA handling production deployments and performs root cause analysis of production errors. Acts as a mentor for interns and junior developers through educational meetings on the latest technologies, coding best practices, and innovative internal tooling."
+				"Leads the software development team for IBM’s cloud-native ledger software. Works across cross-functional teams to enhance the journaling software. Leverages modern technologies to bring a new modern face to IBM's ledger application. Integrated Apache POI to dynamically handle Excel document generation and parsing. Incorporated IBM Watson to construct a full stack chatbot application to handle user queries. Utilized Box and Java Springboot to stream video/image content to chatbot. Incorporated JWT token authentication for meaningful user context. Led performance improvements initiative which led to an overall performance gain of ~60%. Acts as a software librarian for IBM's departmental software handing production deployments."
 		},
 		{
 			image: 'ibm.png',
@@ -45,7 +45,7 @@ const config = {
 			duration: 'June 2019 - May 2021',
 			location: 'Southbury, CT',
 			description:
-				"Full-stack software engineer focused primarily on the front-end and back-end of the IBM cloud-native applications GLUI, the single-entry point to IBM’s ledger for all global journal activity and GUDA, the global application for all IBM departmental activities. Works with testers, PO's, IM's, UI/UX designers, and users to enhance IBM's cloud-native journaling and departmental software. Leverages modern front-end technologies, such as React and Angular, and modern back-end technologies such as Go, GraphQL, Node.js and Express, Java RESTful services using Spring, JDBC, and SQL. Utilizes the IBM DB2 and PostgreSQL relational databases, Docker for containerization, Artifactory for encapsulating all Docker images, IBM Hybrid Cloud and k8s for container orchestration and management, along with a host of CI/CD software to provide automated testing and continuous deployment of cloud-native applications."
+				"Works across cross-functional teams to enhance IBM's ledger and departmental software. Developed various key front-end and back-end services. Integrated IBM Carbon Design system (React) as base design system for IBM's ledger software. Constructed documentation surrounding each of the ledger's micro-service's API utilizing Swagger. Utilized IBM SSO and JWT tokens to securely authenticate users. Integrated dynamic session timeout handling for user sessions. Utilized cron jobs, Hibernate, and Java Springboot to automate the clean-up of user PI data within IBM's departmental software."
 		},
 		{
 			image: 'blackbird.png',
@@ -54,7 +54,7 @@ const config = {
 			duration: 'June 2018 - August 2018',
 			location: 'San Francisco, CA',
 			description:
-				'As a software engineering intern at Blackbird, I worked closely with the CTO and the engineering team to construct custom components for the Blackbird web and mobile apps leveraging React, React Native, React Native Web, GraphQL, Apollo, and ES6 JavaScript. In addition, I provided QA (Quality Assurance) and unit testing on multiple builds of the Blackbird web and mobile apps.'
+				'Worked closely with the CTO and the engineering team to construct custom components for the Blackbird web and mobile apps leveraging React, React Native, React Native Web, GraphQL, and Apollo. Provided Quality Assurance and unit testing on multiple builds of the Blackbird web and mobile apps.'
 		}
 	]
 };
